c6378da839a080a289365dda258dc7f7345d2dd0
guanshuqi <17710293967@163.com>
2019-03-13 08:59:51 +0800

    cmd1

--- api/index.php
+++ api/index.php
@@ -1,5 +1,16 @@
 <?php
+//$cmd="cd /www && ls";
+//$res=shell_exec($cmd);
+//echo $res;
+$web=[
+    '192.168.157.135',
+    '192.168.157.136'
+];
+foreach($web as $k=>$v){
+    $cmd='ssh '.$v . ' "cd /www/cmd && git pull"';
+    echo $cmd;echo "<br>";
+    $res=shell_exec($cmd);
+    echo $res;echo "<hr>";
+}
 
-$cmd="cd /www && ls";
-$res=shell_exec($cmd);
-echo $res;
+
